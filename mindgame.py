62c860f055ed489cccdd6cb72995fd115a7e3f2d
kimyaakbari8-del <kimyaakbari8@gmail.com>
2026-07-18 20:13:54 +0330

    first project

---
+++ mindgame.py
@@ -0,0 +1,11 @@
+print("adad madnazar ro zarb dar 2 kon,hala +8 kon,hasel ro ba adad avali ke madnazaret bood + kon,hala -2 kon,taghsim bar 3,dobare hasel ro menhaie adad aval kon,dar nhaiat zarb dar 4 kon")
+first_number=int(input("choose a number between1-9:"))
+number=first_number
+number*=2
+number+=8
+number+=first_number
+number-=2
+number/=3
+number-=first_number
+number*=4
+print(number,"Is it true?")
